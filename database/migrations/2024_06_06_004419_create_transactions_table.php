1f4363c54afb0529a9f5b8a07e11124481c909b0
alfarezyyd <adityaalfarezyd@gmail.com>
2024-06-07 00:52:17 +0700

    feat: transaction entity

---
+++ database/migrations/2024_06_06_004419_create_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->date('transaction_date')->nullable(false);
+            $table->unsignedInteger('total_price')->nullable(false);
+            $table->unsignedInteger('discount');
+            $table->unsignedInteger('tax')->nullable(false);
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
---
+++ database/migrations/2024_06_06_005548_create_transaction_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaction_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('transaction_id')->nullable(false);
+            $table->unsignedBigInteger('product_id')->nullable(false);
+            $table->unsignedInteger('quantity')->nullable(false);
+            $table->unsignedInteger('price')->nullable(false);
+            $table->unsignedInteger('subtotal')->nullable(false);
+            $table->foreign('transaction_id')->references('id')->on('transactions')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction_items');
+    }
+};
