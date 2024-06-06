272ea2c5c11d61cdcdc4270c16e7d7beb8830280
alfarezyyd <adityaalfarezyd@gmail.com>
2024-06-07 00:50:44 +0700

    feat: add some route

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+  use App\Http\Controllers\CategoryController;
+  use Illuminate\Http\Request;
+  use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+  Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-});
+  });
 
-Route::prefix('products')->group(function () {
-});
+  Route::prefix('products')->group(function () {
+  });
+
+  Route::prefix('categories')->group(function () {
+    Route::get('', [CategoryController::class, 'index']);
+    Route::post('', [CategoryController::class, 'store']);
+  });
